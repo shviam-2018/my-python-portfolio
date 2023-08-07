1abd4f46a39fbbd2701b373228cdf4c2683bd549
Shviam <122033083+shviam-2018@users.noreply.github.com>
2023-08-07 11:46:04 +0200

    add soem more logic to it

--- tp-do.py
+++ tp-do.py
@@ -1,7 +1,20 @@
+import time
+
 my_dict = {"to-do tasks": []}
+
 while True:
     task = input("What have you planned for the day? ")
     if task.lower() == "quit":
         break
     my_dict["to-do tasks"].append(task)
     print(my_dict)
+
+# Get the current time in 24-hour format
+current_time = time.strftime("%H:%M")
+
+if current_time == "00:00":
+    # Clear the to-do tasks list if the time is 00:00 (midnight)
+    my_dict["to-do tasks"] = []
+
+print("Your to-do list for the day:")
+print(my_dict["to-do tasks"])
