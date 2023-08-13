70bccbb24dae29747cdbdcb59039025c15af1e9c
Shviam <122033083+shviam-2018@users.noreply.github.com>
2023-08-13 20:46:57 +0200

    added cat
    upon entering your age it will tell you your age in Dog and cat year.
    
    Co-Authored-By: Aaron Herman Lacerda <122033083+shviam-2018@users.noreply.github.com>

--- human-to-dog-years.py
+++ human-to-dog-years.py
@@ -1,3 +1,5 @@
 HumanYears = int( input("how old are you?: "))
 ToDog = HumanYears * 15
+ToCat = HumanYears * 15
 YouInDogYears = print("your are",ToDog,"is dog years.")
+YouInCatYears = print("you are",ToCat,"is cat years.")
