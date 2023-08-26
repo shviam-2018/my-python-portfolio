3584e0c833e4e46abbac918a8fb15c580bec3815
Aaron Herman Lacerda <shviamjoshi@gmail.com>
2023-08-26 21:03:50 +0200

    fixed maths

--- check leap year.py
+++ check leap year.py
@@ -2,17 +2,10 @@ def is_leap(year):
     leap = False
 
     # Write your logic here
-    if year % 4 == 0:
-        leap = True
-    elif year % 100 == 0:
-        leap = False
-    elif year % 400 == 0:
-        leap = True
-    else:
-        leap = False
+    leap = year % 4 == 0 and year % 100 != 0 or year % 400 == 0
 
     return leap
 
 year = int(input("what year do you what to check: "))
- 
+
 print(is_leap(year))
