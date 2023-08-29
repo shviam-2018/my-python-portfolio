603413ac8b10b65915bc48abca495dcba71972da
Aaron Herman Lacerda <shviamjoshi@gmail.com>
2023-08-29 18:59:53 +0200

    try to optimized

---
+++ counting.py
@@ -0,0 +1,13 @@
+
+        
+if __name__ == '__main__':
+    
+def print_consecutive_numbers(n):
+    for i in range(1, n + 1):
+        print(i, end="")
+        
+    n = int(input(ham))
+    print_consecutive_numbers(n)
+
+
+ham = input("what do you what to count: ")
