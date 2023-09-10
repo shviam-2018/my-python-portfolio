9079f29e674865850cebcb1b959c25ea5d98bc65
Aaron Herman Lacerda <shviamjoshi@gmail.com>
2023-09-10 08:04:33 +0200

    added and fix projects

--- hypertenus.py
+++ hypertenus.py
@@ -1,6 +1,6 @@
 import math
-A = int(input("what is a: "))
-B = int(input("what is b: "))
+A = float(input("what is a: "))
+B = float(input("what is b: "))
  
 first_half = A*A + B*B
 ans = math.sqrt(first_half)
---
+++ repetor.py
@@ -0,0 +1,9 @@
+WTS = input("what do you want me to say: ")
+HT = int(input("how many times should i say this: "))
+
+i=1
+
+while i<=HT:
+    print(i,"",WTS)
+    i= i + 1
+    
